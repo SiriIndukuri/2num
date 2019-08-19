1af1d26d02e97079abd8f0636166f9c607fc63a5
SiriIndukuri <siripavani.indukuri@gmail.com>
2019-08-19 16:15:22 -0400

    commit 1

---
+++ add2num/src/main/java/net/javaguides/springboot/add2num/App.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot.add2num;
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+       SpringApplication.run(App)
+    	System.out.println( "Hello World!" );
+    }
+}
